fc756abb1811d8ac82e204ff0ec563c92a03d392
AnishaDas9 <dasanisha658@gmail.com>
2024-05-05 12:35:41 +0530

    conditionalStatement

---
+++ conditional_statements/condn_stmnts.py
@@ -0,0 +1,133 @@
+# light  ="green"
+
+# if(light == "red"):
+#     print("stop")
+# elif(light == "yellow"):
+#     print("look")
+# elif(light == "green"):
+#     print("go")
+# else:
+#     print("light is broken")
+     
+
+
+                #or
+
+# light = input("light color :")
+
+# if(light == "red"):
+#     print("stop")
+# elif(light == "yellow"):
+#     print("look")
+# elif(light == "green"):
+#     print("go")
+# else:
+#     print("light is broken")
+
+
+
+# EXAMPLE 2*****************************
+
+
+
+# marks = input("marks :")
+# if (marks >= 90):
+#     print("A")
+# elif(marks >= 80 and marks < 90):
+#     print("B")
+# elif(marks >= 70 and marks < 80 ):
+#     print("C")
+# else:
+#     print("D")
+
+
+#********************or*****************
+
+
+
+# marks = 85 
+# if (marks >= 90):
+#     print("A")
+# elif(marks >= 80 and marks < 90):
+#     print("B")
+# elif(marks >= 70 and marks < 80 ):
+#     print("C")
+# else:
+#     print("D")
+
+
+
+#*******************PRACTICE QUESTION********************
+
+
+#***********single line conditional statements***************
+
+
+
+# food = input("food: ")
+
+# eat = "yes" if(food == cake) else "no"
+# print(eat)
+
+
+#********************type conversion***********
+
+
+# a = 2
+# b = 4.25
+# sum = a + b
+# print (sum)
+
+
+#***************TYYPE CASTING****************
+
+# a = int ("2")
+# b = 4.25
+# print(type(a))
+# print(a+ b)
+
+
+#*******************practice question*****************
+
+
+
+# firstnumber = int (input("firstnumber : "))
+# secondnumber =  int (input("secondnumber: "))
+
+# sum = firstnumber + secondnumber
+# print("sum : " , sum)
+
+
+#********************PRACTICE QUESTION 2*******************
+
+
+
+# side = int(input("side :"))
+# area = side*side
+# print("area : " , area)
+
+
+
+#**************PRACTICE QUESTION*********************
+
+
+# firstnumber = float(input("firstnumber :"))
+# secondnumber = float(input("secondnumber :"))
+# sum = firstnumber + secondnumber
+# avg = sum / 2 
+# print("avg :" , avg)
+
+
+
+
+#*****************PRACTICE QUESTION******************
+
+
+
+# a = int(input("a"))
+# b = int (input("b"))
+
+# if (a>= b):
+#     print("true")
+# else:
+#     print("false")

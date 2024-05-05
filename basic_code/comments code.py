141c4b257dcbe500f0942c4565859db6abfa2c09
AnishaDas9 <dasanisha658@gmail.com>
2024-05-05 12:26:49 +0530

    commentText

---
+++ basic_code/comments code.py
@@ -0,0 +1,8 @@
+# hey Anisha please dont remove this line 
+# Author: Anisha 
+# course: 100 days terget
+print("i am a good girl \nand richa is also a good girl")
+print(" hey i am a\" good girl \"\nand richa is also a good girl")
+print("hey",6,7,8,sep="~", end="009\n")
+print("Harry")
+

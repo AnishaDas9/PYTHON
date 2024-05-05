4da8f584dd5dede58156942ad1718b976855edae
AnishaDas9 <dasanisha658@gmail.com>
2024-05-05 12:28:23 +0530

    basic

---
+++ basic_code/basic.py
@@ -0,0 +1,13 @@
+print ("i am Anisha Das" , "my age is 21.") 
+
+# NOTE :      by using comma, we can adjust 2 sentence in one line.Example is above
+
+
+
+# *********************printing numbers*************
+
+
+print(85)
+print(98)
+print(12+28)
+print(96-85)
